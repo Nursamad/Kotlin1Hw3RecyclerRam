20d5296595586dd662692d9a6eddae321bcfc85e
Nursamad <bagishbekow@gmail.com>
2022-01-23 14:44:44 +0600

    add_click

--- app/src/main/java/com/example/kotlin1hw3recycler/ui/MainActivity.kt
+++ app/src/main/java/com/example/kotlin1hw3recycler/ui/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.kotlin1hw3recycler.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlin1hw3recycler.R
 import com.example.kotlin1hw3recycler.databinding.ActivityMainBinding
+import com.example.kotlin1hw3recycler.extension.showToast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapter:RamAdapter
+    private lateinit var adapter: RamAdapter
     private lateinit var binding: ActivityMainBinding
-
     private val img = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,17 +17,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
+
     }
 
     private fun init() {
         binding.apply {
             adapter = RamAdapter(fillList())
             recyclerView.adapter = adapter
+                adapter.setOnItemClickListener(object : RamAdapter.onItemClickListener {
+                    override fun onClick(position: Int) {
+                       showToast(getString(R.string.added))
+                    }
+                })
         }
     }
 
     private fun fillList(): MutableList<String> {
-        (0..20).forEach { _ -> img.add("https://www.meme-arsenal.com/memes/8a6bd2a9a562d83741b68f2c7caaaa40.jpg") }
+        (0..4).forEach { _ ->
+            img.add("https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Mercedes-Logo.svg/1200px-Mercedes-Logo.svg.png")
+            img.add("https://автолого.рф/wp-content/uploads/bmw-logo-2020-grey.png")
+
+        }
         return img
     }
 }
--- app/src/main/java/com/example/kotlin1hw3recycler/ui/RamAdapter.kt
+++ app/src/main/java/com/example/kotlin1hw3recycler/ui/RamAdapter.kt
@@ -8,24 +8,47 @@ import com.example.kotlin1hw3recycler.R
 import com.example.kotlin1hw3recycler.databinding.ItemRamBinding
 import com.example.kotlin1hw3recycler.extension.load
 
-class RamAdapter(private val list: MutableList<String>) : RecyclerView.Adapter<RamAdapter.ViewHolder>() {
+class RamAdapter(private val list: MutableList<String>) :
+    RecyclerView.Adapter<RamAdapter.ViewHolder>() {
 
+    private lateinit var onClick: onItemClickListener
+
+    interface onItemClickListener {
+        fun onClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener) {
+        onClick = listener
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_ram, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view, onClick)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.onBind(list[position])
     }
 
+
     override fun getItemCount(): Int {
         return list.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View, listener: onItemClickListener) :
+        RecyclerView.ViewHolder(itemView) {
         private val binding = ItemRamBinding.bind(itemView)
+
+        init {
+            itemView.setOnClickListener {
+                listener.onClick(adapterPosition)
+                if (binding.transparentView.visibility == View.GONE) {
+                    binding.transparentView.visibility = View.VISIBLE
+                }
+            }
+
+        }
+
         fun onBind(raw: String) = with(binding) {
             ivRam.load(raw)
         }
